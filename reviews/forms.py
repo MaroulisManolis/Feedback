00dbabef3d6a3a8bec8e05acc0512d860ce71db4
MaroulisManolis <manmaroulis@gmail.com>
2022-12-28 08:36:39 +0200

    Storing data in a Database, using ModelForms & learning about Class Based Views

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,7 +1,27 @@
 from django import forms
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your Name",  max_length=100, error_messages={
-        "required": "Your name must not be empty",
-        "max_length": "Please enter a shorter name!"
-    })
+from .models import Review
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your Name",  max_length=100, error_messages={
+#         "required": "Your name must not be empty",
+#         "max_length": "Please enter a shorter name!"
+#     })
+#     review_text = forms.CharField(label="Give your Feedback", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label="Rate here (1-5)",min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Give your Feedback",
+            "rating": "Rate here (1-5)"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty",
+                "max_length": "Please enter a shorter name!"
+            }
+        }
