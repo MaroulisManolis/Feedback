d971a38fad4b88603b62ff5738b837d356b97578
MaroulisManolis <manmaroulis@gmail.com>
2022-12-31 10:25:07 +0200

    Using TemplateView & ListView

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
@@ -25,6 +28,30 @@ class ReviewView(View):
             "form": form
         })
 
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "Your Review is Created"
+        return context
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+    # def get_queryset(self)
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        selected_review = Review.objects.get(pk=review_id)
+        context["review"] = selected_review
+        return context
